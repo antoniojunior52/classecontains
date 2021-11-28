aa99d50326a7f7e46e8fdd3c361361d2640b4a3f
Antonio Junior <antoniocarlosjuninhojuca@gmail.com>
2021-11-28 18:32:08 -0300

    Contains

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace contains
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "C sharp";
+            string s2 = "l";
+            string s3 = "s";
+
+            Console
+                .WriteLine("Se houver um elemento na sequência dada:{0} ",
+                s1.Contains(s2));
+
+            Console
+                .WriteLine("Se houver um elemento na sequência dada:{0} ",
+                s1.Contains(s3));
+
+        }
+    }
+}
